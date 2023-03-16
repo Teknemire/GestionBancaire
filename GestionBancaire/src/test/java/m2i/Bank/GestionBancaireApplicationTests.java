278cc8b0aeefb731f05ee28c12fc3d11559ea45c
emret <emret@DESKTOP-28LVTJ1>
2023-03-16 15:33:56 +0100

    premier comit

---
+++ GestionBancaire/src/test/java/m2i/Bank/GestionBancaireApplicationTests.java
@@ -0,0 +1,13 @@
+package m2i.Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBancaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
